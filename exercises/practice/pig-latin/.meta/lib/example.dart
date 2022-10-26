0f4daaea8ccb405afdd0821b8321d1ea042afc9f
Michael Horn <hornmichaels@gmail.com>
2022-10-26 09:40:44 -0700

    Update example solution to pass new tests

--- exercises/practice/pig-latin/.meta/lib/example.dart
+++ exercises/practice/pig-latin/.meta/lib/example.dart
@@ -1,34 +1,66 @@
-class PigLatin {
-  String translate(String phrase) {
-    return phrase.split(' ').map(_translateWord).join(' ');
+String translate(String phrase) {
+  if (phrase.trim().isEmpty) {
+    return "";
   }
 
-  String _translateWord(String word) {
-    if (_wordStartsWithVowelLike(word)) return word + "ay";
-    if (_wordStartsWithPrefixes(word, ["thr", "sch"])) {
-      return word.substring(3) + word.substring(0, 3) + "ay";
-    }
+  return phrase.split(' ').map(_translateCapitalizedWord).join(' ');
+}
 
-    if (_wordStartsWithPrefixes(word, ["ch", "qu", "th", "rh"])) {
-      return word.substring(2) + word.substring(0, 2) + "ay";
-    }
+String _translateCapitalizedWord(String word) {
+  final translation = _translateWord(word);
 
-    if (_wordStartsWithConsonantAndQu(word)) {
-      return word.substring(3) + word[0] + "quay";
-    }
+  return _isCapitalized(word) ? _capitalize(translation) : translation;
+}
 
-    return word.substring(1) + word[0] + "ay";
+String _translateWord(String word) {
+  final isAllCaps = _isAllUpperCase(word);
+  final ay = isAllCaps ? "AY" : "ay";
+  final quay = isAllCaps ? "QUAY" : "quay";
+
+  if (_wordStartsWithVowelLike(word)) {
+    return word + ay;
   }
 
-  bool _wordStartsWithVowelLike(String word) {
-    return RegExp(r'^([aeiou]|yt|xr)').hasMatch(word);
+  if (_wordStartsWithPrefixes(word, ["thr", "sch"])) {
+    return word.substring(3) + word.substring(0, 3) + ay;
   }
 
-  bool _wordStartsWithPrefixes(String word, List<String> prefixes) {
-    return prefixes.any((prefix) => word.startsWith(prefix));
+  if (_wordStartsWithPrefixes(word, ["ch", "qu", "th", "rh"])) {
+    return word.substring(2) + word.substring(0, 2) + ay;
   }
 
-  bool _wordStartsWithConsonantAndQu(String word) {
-    return RegExp(r'[bcdfghjklmnpqrstvwxyz]qu').hasMatch(word);
+  if (_wordStartsWithConsonantAndQu(word)) {
+    return word.substring(3) + word[0] + quay;
   }
+
+  return word.substring(1) + word[0] + ay;
+}
+
+String _capitalize(String word) {
+  return "${word[0].toUpperCase()}${word.substring(1).toLowerCase()}";
+}
+
+bool _isCapitalized(String word) {
+  return RegExp(r'^[A-Z][a-z]+$').hasMatch(word);
+}
+
+bool _isAllUpperCase(String word) {
+  return word == word.toUpperCase();
+}
+
+bool _wordStartsWithVowelLike(String word) {
+  return RegExp(r'^([aeiou]|yt|xr)', caseSensitive: false).hasMatch(word);
+}
+
+bool _wordStartsWithPrefixes(String word, List<String> prefixes) {
+  return prefixes.any(
+    (prefix) => word.startsWith(RegExp(prefix, caseSensitive: false)),
+  );
+}
+
+bool _wordStartsWithConsonantAndQu(String word) {
+  return RegExp(
+    r'[bcdfghjklmnpqrstvwxyz]qu',
+    caseSensitive: false,
+  ).hasMatch(word);
 }
